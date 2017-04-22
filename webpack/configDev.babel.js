77002896faa6413235112fb6ec51a2c821c9cec8
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2017-04-22 05:55:09 -0700

    build: webpack-dev-server with HMR

--- webpack/configDev.babel.js
+++ webpack/configDev.babel.js
@@ -1,12 +1,21 @@
-import baseConfig from './configBase.babel'
+import webpack from 'webpack'
 
+import baseConfig from './configBase.babel'
 import { dist } from './filePaths'
 
 const config = {
   ...baseConfig,
   devServer: {
-    contentBase: dist
-  }
+    contentBase: dist,
+    publicPath: '/assets',
+    port: process.env.PORT || 3000
+  },
+  plugins: [
+    ...baseConfig.plugins,
+    new webpack.HotModuleReplacementPlugin()
+  ]
 }
+
+console.log(config)
 
 export default config
