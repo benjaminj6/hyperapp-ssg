897e42906915533412cdcd4dcb114984ede5c978
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2017-04-22 05:22:46 -0700

    build: adding a dev build with webpack-dev-server

---
+++ webpack/file-paths.js
@@ -0,0 +1,6 @@
+import path from 'path'
+
+export const root = path.resolve(__dirname, '..')
+export const dist = path.resolve(root, 'dist')
+export const statics = path.resolve(root, 'statics')
+export const src = path.resolve(root, 'src')
