1f777c8e9fdfd81c27cda94c5d7e35b6655e9e5e
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2017-04-25 14:34:42 -0700

    theme: get styled components binding working

--- src/view/index.js
+++ src/view/index.js
@@ -2,7 +2,15 @@
 import { h } from 'hyperapp' // eslint-disable-line
 
 import type { State } from '../state'
+import styled from 'packages/styled-components'
+
+const Title = styled('h1')`
+  background: ${props => props.bg}
+  color: white
+  font-family: sans-serif
+  padding: 5px 20px
+`
 
 export default (state: State) => (
-  <h1 class='a'>test</h1>
+  <Title bg='blue'>foo</Title>
 )
